2c9a61e7850973480ac359b39f55c256b79ab302
Praveen <pkchmits@gmail.com>
2021-05-22 21:31:57 +0530

    Removed unused code

--- app/src/main/java/com/example/quizapp/Question.java
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -1,12 +1,9 @@
 package com.example.quizapp;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Question {
-    private String question;
-    private String[] options;
-    private String[] answers;
+    private final String question;
+    private final String[] options;
+    private final String[] answers;
 
     public Question(String question, String[] options, String[] answers) {
         this.question = question;
@@ -18,33 +15,13 @@ public class Question {
         return question;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
 
     public String[] getOptions() {
         return options;
-    }
-
-    public void setOptions(String[] options) {
-        this.options = options;
     }
 
     public String[] getAnswers() {
         return answers;
     }
 
-    public void setAnswers(String[] answers) {
-        this.answers = answers;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "question='" + question + '\'' +
-                ", options=" + Arrays.toString(options) +
-                ", answers=" + Arrays.toString(answers) +
-                '}';
-    }
 }
